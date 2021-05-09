4d071cc8cb7073682e940bafe76d2121d6171885
marbinuwusito <tujamoxd123@gmail.com>
2021-05-09 12:19:31 -0600

    xd

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.set('port', port);
 
-app.use(express.urlencoded({ extended : false }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 module.exports = app;
