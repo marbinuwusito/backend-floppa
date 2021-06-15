644c5050d435d0ad3b7923595c9b7301d6fdcbb6
marbinuwusito <tujamoxd123@gmail.com>
2021-06-14 18:45:21 -0600

    finish

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,26 +1,24 @@
-const mysql = require('mysql');
+import mysql from 'mysql';
 const data = require('./data');
 
 const objectConnection = {
 
-   host: data.mysql.host,
-   port: data.mysql.port,
-   user: data.mysql.user,
-   password: data.mysql.pass,
-   database: data.mysql.database
+  host : data.mysql.host,
+  port : data.mysql.port,
+  user : data.mysql.user,
+  password : data.mysql.pass,
+  database : data.mysql.database
 
 }
 
 const myConfig = mysql.createConnection(objectConnection);
 
 myConfig.connect((err) => {
-
-   if (err) {
-      console.log(`there was an error: ${err}`);
-   } else {
-      console.log("database connected successfully");
-   }
-
+  if (err) {
+    console.log(`there was an error: ${err}`);
+  } else {
+    console.log("database connected successfully");
+  }
 });
 
 module.exports = myConfig;
--- src/helpers/imageUploads.js
+++ src/helpers/imageUploads.js
@@ -1,12 +1,14 @@
-const multer = require('multer');
+import multer from 'multer';
 
 const storage = multer.diskStorage({
-   destination: function (req, file, cb) {
-      cb(null, './src/storage')
-   },
-   filename: function (req, file, cb) {
-      cb(null, `${file.fieldname}-${Date.now()}.png`)
-   }
+
+  destination: function (req, file, cb) {
+    cb(null, './src/storage')
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${file.fieldname}-${Date.now()}.png`)
+  }
+
 });
 
 const upload = multer({ storage });
