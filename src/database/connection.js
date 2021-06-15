f18ff58be49b2172cb9c680fe36847ff77e3ba62
marbinuwusito <tujamoxd123@gmail.com>
2021-06-14 18:45:56 -0600

    finish

--- src/database/connection.js
+++ src/database/connection.js
@@ -2,13 +2,11 @@ import mysql from 'mysql';
 const data = require('./data');
 
 const objectConnection = {
-
   host : data.mysql.host,
   port : data.mysql.port,
   user : data.mysql.user,
   password : data.mysql.pass,
   database : data.mysql.database
-
 }
 
 const myConfig = mysql.createConnection(objectConnection);
